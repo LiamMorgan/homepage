330400aa9512a727a6df564691e16176cfa1576b
Liam Morgan <liammorgan128@gmail.com>
2025-11-26 09:35:49 +0000

    Fixing build and lint issues

--- src/widgets/posterizarr/component.jsx
+++ src/widgets/posterizarr/component.jsx
@@ -1,5 +1,6 @@
 import Block from "components/services/widget/block";
 import Container from "components/services/widget/container";
+
 import useWidgetAPI from "utils/proxy/use-widget-api";
 
 const fieldNames = {
