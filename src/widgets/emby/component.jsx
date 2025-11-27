d2e50d36bbcc4999df8c233e2ac1f5c6f07817f7
Liam Morgan <liammorgan128@gmail.com>
2025-11-27 12:46:42 +0000

    Fix task progress display in TaskEntry component: use default value for progress and adjust layout

--- src/widgets/emby/component.jsx
+++ src/widgets/emby/component.jsx
@@ -175,20 +175,22 @@ function SessionEntry({ playCommand, session, enableUser, showEpisodeNumber, ena
 }
 
 function TaskEntry({ task }) {
+  const progress = task.progress || 0;
+
   return (
     <div className="text-theme-700 dark:text-theme-200 relative h-5 w-full rounded-md bg-theme-200/50 dark:bg-theme-900/20 mt-1 flex">
       <div
         className="absolute h-5 rounded-md bg-theme-200 dark:bg-theme-900/40 z-0"
         style={{
-          width: `${task.progress}%`,
+          width: `${progress}%`,
         }}
       />
-      <div className="grow text-xs z-10 self-center relative w-full h-4 ml-6">
+      <div className="grow text-xs z-10 self-center relative w-full h-4 ml-1">
         <div className="absolute w-full whitespace-nowrap text-ellipsis overflow-hidden" title={`Task: ${task.name}`}>
           {`Task: ${task.name}`}
         </div>
       </div>
-      <div className="self-center text-xs flex justify-end mr-6 z-10">{`${task.progress.toFixed(2)}%`}</div>
+      <div className="self-center text-xs flex justify-end mr-6 z-10">{`${progress.toFixed(2)}%`}</div>
     </div>
   );
 }
